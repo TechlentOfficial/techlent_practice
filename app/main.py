a53a4a7556d198f211fbcfe9c249548b8366a4f2
Techlent <techlent@MacBook-Air.local>
2020-12-26 16:15:37 -0800

    flask app

--- app/main.py
+++ app/main.py
@@ -1,5 +1,34 @@
 from src.calculator import Calculator
+from flask import Flask, render_template, request
+
 
 cal = Calculator()
+app = Flask(__name__)
 
-print(cal.add(1, 2))
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    result = ""
+    if request.method == "POST":
+        num1 = request.form["num1"]
+        num2 = request.form["num2"]
+        operator = request.form["operator"]
+        if operator == "+":
+            result = cal.add(float(num1), float(num2))
+        elif operator == "-":
+            result = cal.subtract(float(num1), float(num2))
+        elif operator == "*":
+            result = cal.multiply(float(num1), float(num2))
+        else:
+            result = cal.divide(float(num1), float(num2))
+
+    return render_template("index.html", result=result)
+
+
+@app.route("/health")
+def health():
+    return "I am healthy"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='127.0.0.1', port=5000)
